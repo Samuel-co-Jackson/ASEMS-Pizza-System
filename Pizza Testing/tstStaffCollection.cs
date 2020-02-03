c909107ad8d76f2245b622f2995deccb73aac9e0
LanternCode <Adam.Machowczyk@gmail.com>
2020-02-03 15:40:38 +0000

    Added a collection class for Staff

---
+++ Pizza Testing/tstStaffCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzaClasses;
+
+namespace Pizza_Testing
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection allStaff = new clsStaffCollection();
+            //test to see that it exists
+            Assert.IsNotNull(allStaff);
+        }
+    }
+}
