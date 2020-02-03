a9eece6cbb731b013290adff0d65ac59d9d1d0c1
Samuel <P2422681@my365.dmu.ac.uk>
2020-02-03 15:04:13 +0000

    Added properties for testing to tstMenu and clsMenu

--- Pizza Testing/tstMenu.cs
+++ Pizza Testing/tstMenu.cs
@@ -15,5 +15,70 @@ namespace Pizza_Testing
             //test to see that it exists
             Assert.IsNotNull(AMenu);
         }
+
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsMenu AMenu = new clsMenu();
+            //create some test data to assign to the property
+            String TestData = "Cheese";
+            //assign the data to the property
+            AMenu.Name = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AMenu.Name, TestData);
+        }
+
+        [TestMethod]
+        public void MenuItemIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsMenu AMenu = new clsMenu();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AMenu.MenuItemID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AMenu.MenuItemID, TestData);
+        }
+
+        [TestMethod]
+        public void DescriptionPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsMenu AMenu = new clsMenu();
+            //create some test data to assign to the property
+            String TestData = "SUPER CHEESY all natural cheese!";
+            //assign the data to the property
+            AMenu.Description = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AMenu.Description, TestData);
+        }
+
+        [TestMethod]
+        public void RecipeIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsMenu AMenu = new clsMenu();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AMenu.RecipeID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AMenu.RecipeID, TestData);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsMenu AMenu = new clsMenu();
+            //create some test data to assign to the property
+            float TestData = 2;
+            //assign the data to the property
+            AMenu.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AMenu.Price, TestData);
+        }
     }
 }
