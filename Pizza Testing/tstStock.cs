2b2f3964399c8c1d498e23ead2e832ca7a5eeeb9
P2446677 <P2446677@GWHXX473WIN016W>
2020-02-03 15:07:42 +0000

    Added a Stock class and tstStock test and created test InstanceOK()

---
+++ Pizza Testing/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzaClasses;
+
+namespace Pizza_Testing
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class I want to create
+            clsStock AStock = new clsStock();
+            //test to see if it exists
+            Assert.IsNotNull(AStock);
+        }
+    }
+}
