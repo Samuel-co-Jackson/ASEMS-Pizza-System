fb08519717c40db4dbe11a6742431a431312bba6
P2421782 <P2421782@my365.dmu.ac.uk>
2020-02-04 19:51:58 +0000

    Completed testing and class for my customer managment properties

--- PizzaClasses/clsCustomer.cs
+++ PizzaClasses/clsCustomer.cs
@@ -2,5 +2,15 @@
 {
     public class clsCustomer
     {
+        public bool Active { get; set; }
+        public int CustomerID { get; set; }
+        public string Customerfirstname { get; set; }
+        public string Customerlastname { get; set; }
+        public string Customerhouseno { get; set; }
+        public string Customerstreetname { get; set; }
+        public string Customercity { get; set; }
+        public string Customerpostcode { get; set; }
+        public string Customeremail { get; set; }
+        public long Customerphoneno { get; set; }
     }
 }
