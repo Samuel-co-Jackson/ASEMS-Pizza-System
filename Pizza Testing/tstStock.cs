383aa27ce43ba03e0a19d1c2cb7b5795309af558
P2446677 <P2446677@GWHXX473WIN016W>
2020-02-05 12:49:57 +0000

    Added a stock presentation layer (without code)

--- Pizza Testing/tstStock.cs
+++ Pizza Testing/tstStock.cs
@@ -15,5 +15,56 @@ namespace Pizza_Testing
             //test to see if it exists
             Assert.IsNotNull(AStock);
         }
-    }
+
+        [TestMethod]
+        public void NameOK()
+        {
+            //create an instance of the class I want to create
+            clsStock AStock = new clsStock();
+            //create test data to assign to property
+            string TestData = "Cucumber";
+            //assign the data to the property
+            AStock.Name = TestData;
+            //test to see if the two values match
+            Assert.AreEqual(AStock.Name, TestData);
+        }
+
+        [TestMethod]
+        public void DescriptionOK()
+        {
+            //create an instance of the class I want to create
+            clsStock AStock = new clsStock();
+            //create test data to assign to property
+            string TestData = "Cucumber";
+            //assign the data to the property
+            AStock.Description = TestData;
+            //test to see if the two values match
+            Assert.AreEqual(AStock.Description, TestData);
+        }
+
+        [TestMethod]
+        public void StockQuantityOK()
+        {
+            //create an instance of the class I want to create
+            clsStock AStock = new clsStock();
+            //create test data to assign to property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStock.StockQuantity = TestData;
+            //test to see if the two values match
+            Assert.AreEqual(AStock.StockQuantity, TestData);
+        }
+
+        public void StockItemPriceOK()
+        {
+            //create an instance of the class I want to create
+            clsStock AStock = new clsStock();
+            //create test data to assign to property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStock.StockItemPrice = TestData;
+            //test to see if the two values match
+            Assert.AreEqual(AStock.StockItemPrice, TestData);
+        }
+    }//srghwrwrgt
 }
---
+++ PizzaFrontOffice/AStock.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AStock : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ButtonOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        //clsStock AStock = new clsStock();
+
+    }
+}
