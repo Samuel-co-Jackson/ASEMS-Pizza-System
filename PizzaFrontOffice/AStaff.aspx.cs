ef960de620c22803219dc276adc89921d6e01b93
LanternCode <Adam.Machowczyk@gmail.com>
2020-02-10 15:59:28 +0000

    Added the presentation layer for staff

---
+++ PizzaFrontOffice/AStaff.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PizzaClasses;
+
+public partial class AStaff : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSave_Click1(object sender, EventArgs e)
+    {
+        //create a new instance of clsStaff
+        clsStaff AStaff = new clsStaff();
+        //capture the staff name
+        AStaff.staffName = txtName.Text;
+        //store the name in the session object
+        Session["AStaff"] = AStaff;
+        //redirect to the viewer page
+        Response.Redirect("StaffViewer.aspx");
+    }
+}
