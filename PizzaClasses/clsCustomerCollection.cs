0f96f7929d43505dbc8ccc62da00f7d75897d11b
P2421782 <P2421782@my365.dmu.ac.uk>
2020-02-13 17:50:25 +0000

    Added two records present and constuctor.

--- PizzaClasses/clsCustomerCollection.cs
+++ PizzaClasses/clsCustomerCollection.cs
@@ -7,6 +7,42 @@ namespace PizzaClasses
         //private data member for the list
         List<clsCustomer> mCustomerList = new List<clsCustomer>();
 
+        //constructor for the class
+        public clsCustomerCollection()
+        {
+            //creae the items of test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its properties
+            TestItem.Active = true;
+            TestItem.CustomerID = 1;
+            TestItem.Customerfirstname = "Sufiyaan";
+            TestItem.Customerlastname = "Hussain";
+            TestItem.Customerhouseno = "34B";
+            TestItem.Customerstreetname = "Gateway House";
+            TestItem.Customercity = "Leicester";
+            TestItem.Customerpostcode = "LE1 5GH";
+            TestItem.Customerphoneno = 07806652976;
+            TestItem.Customeremail = "SufiyaanDMU@hotmail.co.uk";
+            //add the item to the test list
+            mCustomerList.Add(TestItem);
+            //re initialise the object from some new data
+            TestItem = new clsCustomer();
+            //set its properties
+            TestItem.Active = true;
+            TestItem.CustomerID = 1;
+            TestItem.Customerfirstname = "Tony";
+            TestItem.Customerlastname = "Martial";
+            TestItem.Customerhouseno = "10";
+            TestItem.Customerstreetname = "Old Trafford";
+            TestItem.Customercity = "Manchester";
+            TestItem.Customerpostcode = "MN1 3AJ";
+            TestItem.Customerphoneno = 01234567892;
+            TestItem.Customeremail = "TonyMartial@hotmail.com";
+            //add the item to the test list
+            mCustomerList.Add(TestItem);
+        }
+
+
         //public property for the customer list
         public List<clsCustomer> CustomerList
         {
@@ -37,6 +73,9 @@ namespace PizzaClasses
 
         //public List<clsCustomer> CustomerList { get; set; }
         public clsCustomer ThisCustomer { get; set; }
+
     }
+
+
     
 }
