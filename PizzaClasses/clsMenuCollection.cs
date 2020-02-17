3660e523ad53b254967ba05303c4394dd9e44234
Samuel <P2422681@my365.dmu.ac.uk>
2020-02-17 15:41:48 +0000

    completed week 20 work on menu collection class using database

--- PizzaClasses/clsMenuCollection.cs
+++ PizzaClasses/clsMenuCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace PizzaClasses
 {
@@ -35,6 +36,38 @@ namespace PizzaClasses
                 //worry about this later
             }
         }
+
         public clsMenu ThisMenuItem { get; set; }
+
+        //constructor for this class
+        public clsMenuCollection()
+        {
+            //var for the index
+            int Index = 0;
+            //var to store the record count
+            int RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblMenu_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while(Index < RecordCount)
+            {
+                //create a blank Menu
+                clsMenu AMenu = new clsMenu();
+                //read in the fields from the current record
+                AMenu.MenuItemID = Convert.ToInt32(DB.DataTable.Rows[Index]["MenuID"]);
+                AMenu.Name = Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
+                AMenu.Description = Convert.ToString(DB.DataTable.Rows[Index]["Description"]);
+                AMenu.RecipeID = Convert.ToInt32(DB.DataTable.Rows[Index]["RecipeID"]);
+                AMenu.Price = Convert.ToDouble(DB.DataTable.Rows[Index]["Price"]);
+                //add the record to the private data member
+                mMenuList.Add(AMenu);
+                //point at the next record
+                Index++;
+            }
+        }
     }
 }
