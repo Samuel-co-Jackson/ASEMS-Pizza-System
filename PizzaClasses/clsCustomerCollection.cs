f7fb9e10f717b718491ba9b8da92dd76c2afe163
P2421782 <P2421782@my365.dmu.ac.uk>
2020-02-17 16:32:20 +0000

    Added Azure DataConnection and commented out a few tests. All test working and passed.

--- PizzaClasses/clsCustomerCollection.cs
+++ PizzaClasses/clsCustomerCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace PizzaClasses
 {
@@ -6,42 +7,7 @@ namespace PizzaClasses
     {
         //private data member for the list
         List<clsCustomer> mCustomerList = new List<clsCustomer>();
-
-        //constructor for the class
-        public clsCustomerCollection()
-        {
-            //creae the items of test data
-            clsCustomer TestItem = new clsCustomer();
-            //set its properties
-            TestItem.Active = true;
-            TestItem.CustomerID = 1;
-            TestItem.Customerfirstname = "Sufiyaan";
-            TestItem.Customerlastname = "Hussain";
-            TestItem.Customerhouseno = "34B";
-            TestItem.Customerstreetname = "Gateway House";
-            TestItem.Customercity = "Leicester";
-            TestItem.Customerpostcode = "LE1 5GH";
-            TestItem.Customerphoneno = 07806652976;
-            TestItem.Customeremail = "SufiyaanDMU@hotmail.co.uk";
-            //add the item to the test list
-            mCustomerList.Add(TestItem);
-            //re initialise the object from some new data
-            TestItem = new clsCustomer();
-            //set its properties
-            TestItem.Active = true;
-            TestItem.CustomerID = 1;
-            TestItem.Customerfirstname = "Tony";
-            TestItem.Customerlastname = "Martial";
-            TestItem.Customerhouseno = "10";
-            TestItem.Customerstreetname = "Old Trafford";
-            TestItem.Customercity = "Manchester";
-            TestItem.Customerpostcode = "MN1 3AJ";
-            TestItem.Customerphoneno = 01234567892;
-            TestItem.Customeremail = "TonyMartial@hotmail.com";
-            //add the item to the test list
-            mCustomerList.Add(TestItem);
-        }
-
+        public clsCustomer ThisCustomer { get; set; }
 
         //public property for the customer list
         public List<clsCustomer> CustomerList
@@ -57,6 +23,7 @@ namespace PizzaClasses
                 mCustomerList = value;
             }
         }
+
         //public property for count
         public int Count
         {
@@ -71,11 +38,43 @@ namespace PizzaClasses
             }
         }
 
+        //constructor for the class
+        public clsCustomerCollection()
+        {
+            //var to store the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //excute the stored procedure
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            //get the count of the records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank customer
+                clsCustomer ACustomer = new clsCustomer();
+                //read in the fields from the current record
+                //AnCustomer.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                ACustomer.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                ACustomer.Customerfirstname = Convert.ToString(DB.DataTable.Rows[Index]["Firstname"]);
+                ACustomer.Customerlastname = Convert.ToString(DB.DataTable.Rows[Index]["Lastname"]);
+                ACustomer.Customerhouseno = Convert.ToString(DB.DataTable.Rows[Index]["HouseNo"]);
+                ACustomer.Customerstreetname = Convert.ToString(DB.DataTable.Rows[Index]["Streetname"]);
+                ACustomer.Customercity = Convert.ToString(DB.DataTable.Rows[Index]["City"]);
+                ACustomer.Customerpostcode = Convert.ToString(DB.DataTable.Rows[Index]["Postcode"]);
+                ACustomer.Customeremail = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                ACustomer.Customerphoneno = Convert.ToInt64(DB.DataTable.Rows[Index]["Phone No"]);
+                //add the record to the private data member
+                mCustomerList.Add(ACustomer);
+                //point to the next record
+                Index++;
+            }
+        }
+
         //public List<clsCustomer> CustomerList { get; set; }
-        public clsCustomer ThisCustomer { get; set; }
 
     }
-
-
-    
 }
