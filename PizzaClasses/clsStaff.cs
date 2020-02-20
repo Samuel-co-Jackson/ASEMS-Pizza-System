b214274dc3f165ab1500227fe64d662a3d680f1f
LanternCode <Adam.Machowczyk@gmail.com>
2020-02-20 11:52:40 +0000

    Added the "Find" method for clsStaff

--- PizzaClasses/clsStaff.cs
+++ PizzaClasses/clsStaff.cs
@@ -4,15 +4,48 @@ namespace PizzaClasses
 {
     public class clsStaff
     {
+
+        private Int32 mStaffId;
+        private String mStaffName;
+        private String mStaffAddress;
+        private DateTime mStaffHiredOn;
+        private int mStaffRoleId;
+
         public string staffAddress { get; set; }
         public DateTime staffHiredOn { get; set; }
-        public int staffId { get; set; }
+        public int staffId 
+        {
+            get
+            {
+                return mStaffId;
+            }
+            set
+            {
+                mStaffId = value;
+            }
+               
+        }
         public string staffName { get; set; }
         public int staffRoleId { get; set; }
 
-        public void Find(int staffId)
+        public bool Find(int staffId)
         {
-            throw new NotImplementedException();
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@staffId", staffId);
+            DB.Execute("sproc_tblStaff_FilterByStaffId");
+            if (DB.Count == 1)
+            {
+                mStaffId = Convert.ToInt32(DB.DataTable.Rows[0]["staffId"]);
+                mStaffName = Convert.ToString(DB.DataTable.Rows[0]["staffName"]);
+                mStaffAddress = Convert.ToString(DB.DataTable.Rows[0]["staffAddress"]);
+                mStaffHiredOn = Convert.ToDateTime(DB.DataTable.Rows[0]["staffHiredOn"]);
+                mStaffRoleId = Convert.ToInt32(DB.DataTable.Rows[0]["staffRoleId"]);
+                return true;
+            }
+            else return false;
+
         }
     }
 }
