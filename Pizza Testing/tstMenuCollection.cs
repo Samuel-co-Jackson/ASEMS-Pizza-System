60a3d5c524f29a7cfc47047f90aaaf7129864a9f
Samuel <P2422681@my365.dmu.ac.uk>
2020-02-25 15:48:39 +0000

    Added stored procedures for Add

--- Pizza Testing/tstMenuCollection.cs
+++ Pizza Testing/tstMenuCollection.cs
@@ -87,5 +87,31 @@ namespace Pizza_Testing
             //test to see that the two values are the same
             Assert.AreEqual(AllMenu.Count, TestList.Count);
         }
+
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsMenuCollection AllMenu = new clsMenuCollection();
+            //create the item of test data 
+            clsMenu TestItem = new clsMenu();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set the properties
+            TestItem.Name = "Cheesy";
+            TestItem.Description = "Classic cheese delite!";
+            TestItem.RecipeID = 1;
+            TestItem.Price = 5.0;
+            //set ThisMenu to test data
+            AllMenu.ThisItem = TestItem;
+            //add the record
+            PrimaryKey = AllMenu.Add();
+            //set the primary key of the test data
+            TestItem.MenuItemID = PrimaryKey;
+            //find the record
+            AllMenu.ThisItem.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllMenu.ThisItem, TestItem);
+        }
     }
 }
